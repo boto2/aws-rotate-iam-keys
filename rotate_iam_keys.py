c0e9c6710ec4bba38a1b98eb154230981895b638
r2 <r2@r2.local>
2020-01-26 21:16:51 -0500

    Added first version of rotate_iam_keys python script

---
+++ rotate_iam_keys.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import boto3
+import argparse
+
+USERS_TO_EXCLUDE = ['jenkins']
+
+
+def get_all_users(iam):
+    all_users = []
+    for user_list in iam.list_users().get('Users'):
+        all_users.append(user_list['UserName'])
+    return all_users
+
+
+def delete_keys(users, iam):
+    for user in users:
+        if user not in USERS_TO_EXCLUDE:
+            rotate_keys_for_user(user_name=user, iam=iam)
+
+
+def rotate_keys_for_user(user_name, iam):
+    all_keys = iam.list_access_keys(UserName=user_name).get("AccessKeyMetadata")
+    if all_keys is not None:
+        for key in all_keys:
+            key_id = key.get("AccessKeyId")
+            print "Deleting key {} for user {}".format(key_id, user_name)
+            iam.delete_access_key(UserName=user_name, AccessKeyId=key_id)
+        print "Creating a new key for user {}".format(user_name)
+        iam.create_access_key(UserName=user_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--profile-name',
+                        default=None,
+                        help='The aws profile name')
+    aws_profile_name = parser.parse_args().profile_name
+    session = boto3.Session(profile_name=aws_profile_name)
+    iam_client = session.client('iam')
+
+    all_users = get_all_users(iam=iam_client)
+    delete_keys(users=all_users, iam=iam_client)
