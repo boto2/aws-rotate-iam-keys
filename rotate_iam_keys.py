ce9f875c76e46e59734a18b6ad511f66b4f56c22
r2 <r2@r2.fios-router.home>
2020-03-22 13:44:08 -0400

    initial commit

--- rotate_iam_keys.py
+++ rotate_iam_keys.py
@@ -3,29 +3,37 @@
 
 import boto3
 import argparse
+import json
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.credential import AmazonWebServicesCredentials
 
 
-AWS_USER_TO_UPDATE = 'test1'
+AWS_USER_TO_UPDATE = ""
+AWS_JENKINS_USER_PARAMETER_STORE = "/ServersInfra/Jenkins/UserName"
+AWS_JENKINS_PASSWORD_PARAMETER_STORE = "/ServersInfra/Jenkins/Password"
+S3_BUCKET_NAME = "personal-storage-mike"
 
+def get_parameter_store_value(parameter_key, session):
+    ssm_client = session.client('ssm')
+    res = ssm_client.get_parameter(Name=parameter_key, WithDecryption=False)
+    return res.get('Parameter').get('Value')
 
 def get_all_users(iam):
     all_users = []
-    for user_list in iam.list_users().get('Users'):
-        all_users.append(user_list['UserName'])
+    for user_list in iam.list_users().get("Users"):
+        all_users.append(user_list["UserName"])
     return all_users
 
 
-def delete_keys(users, iam, jenkins_conn, jenkins_credentials_description, aws_user_to_update):
+def delete_keys(users, iam, jenkins_conn, jenkins_credentials_description, aws_user_to_update, s3_client):
     for user in users:
         if user == aws_user_to_update:
-            rotate_keys_for_user(user_name=user, iam=iam, jenkins_conn=jenkins_conn, jenkins_credentials_description=jenkins_credentials_description, aws_user_to_update=aws_user_to_update)
+            rotate_keys_for_user(user_name=user, iam=iam, jenkins_conn=jenkins_conn, jenkins_credentials_description=jenkins_credentials_description, aws_user_to_update=aws_user_to_update, s3_client=s3_client)
         else:
-	    print "Skipping user {}".format(user)
+            print "Skipping user {}".format(user)        
 
 
-def rotate_keys_for_user(user_name, iam, jenkins_conn, jenkins_credentials_description, aws_user_to_update):
+def rotate_keys_for_user(user_name, iam, jenkins_conn, jenkins_credentials_description, aws_user_to_update, s3_client):
     try:
         all_keys = iam.list_access_keys(UserName=user_name).get("AccessKeyMetadata")
         if all_keys is not None:
@@ -34,37 +42,51 @@ def rotate_keys_for_user(user_name, iam, jenkins_conn, jenkins_credentials_descr
                 print "Deleting key {} for user {}".format(key_id, user_name)
                 iam.delete_access_key(UserName=user_name, AccessKeyId=key_id)
             print "Creating a new key for user {}".format(user_name)
-            res = iam.create_access_key(UserName=user_name)
+            res = iam.create_access_key(UserName=user_name)                                     
             if user_name == aws_user_to_update:
                 access_key = res.get("AccessKey")
-                key_id = access_key.get('AccessKeyId')
-                secret_key = access_key.get('SecretAccessKey')
-                creds = j.credentials
+                key_id = access_key.get("AccessKeyId")
+                secret_key = access_key.get("SecretAccessKey")                
+                user_dict = {
+                    "access_key": str(access_key),
+                    "key_id": str(key_id),
+                    "secret_key": str(secret_key)
+                }
+                with open('aws_creds.json', 'a') as f:
+                    f.write(json.dumps(user_dict, indent=4))
+                    s3_client.list_objects(Bucket=S3_BUCKET_NAME)
+                    print "Uploading the user credentials to {}".format(S3_BUCKET_NAME)
+                    s3_client.upload_file('aws_creds.json', S3_BUCKET_NAME, 'aws_creds.json')
+
+                creds = j.credentials                               
                 aws_creds = {
-                    'description': jenkins_credentials_description,
-                    'accessKey': key_id,
-                    'secretKey': secret_key
+                    "description": jenkins_credentials_description,
+                    "accessKey": key_id,
+                    "secretKey": secret_key
                 }
                 print "Updating Jenkins credentials {} with the AWS user name {} and with the key ID {}".format(jenkins_credentials_description, user_name, key_id)
                 creds[jenkins_credentials_description] = AmazonWebServicesCredentials(aws_creds)
 
     except Exception as e:
         print "There was an error"
+        print "{}".format(e)
+        raise e
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--profile-name',
                         default=None,
                         help='The aws profile name')
-    parser.add_argument('-u', '--jenkins-user',
-                        default=None,
-                        required=True,
-                        help='The jenkins user name')
-    parser.add_argument('-t', '--jenkins-password',
-                        default=None,
-                        required=True,
-                        help='The jenkins password')
+    # parser.add_argument('-u', '--jenkins-user',
+    #                     default=None,
+    #                     required=True,
+    #                     help='The jenkins user name')
+    # parser.add_argument('-t', '--jenkins-password',
+    #                     default=None,
+    #                     required=True,
+    #                     help='The jenkins password')
     parser.add_argument('-c', '--credentials-description',
                         default=None,
                         required=True,
@@ -72,21 +94,20 @@ if __name__ == '__main__':
     parser.add_argument('--aws-user-to-update',
                         required=True,
                         help='The aws user to update')
-    parser.add_argument('--ssl-verify',
-                        required=False,
-                        action='store_true',
-                        help='Provide this argument if you want to use ssl verification')
+
     aws_profile_name = parser.parse_args().profile_name
-    jenkins_user = parser.parse_args().jenkins_user
-    jenkins_password = parser.parse_args().jenkins_password
+    # jenkins_user = parser.parse_args().jenkins_user
+    # jenkins_password = parser.parse_args().jenkins_password
     jenkins_credentials_description = parser.parse_args().credentials_description
     aws_user_to_update = parser.parse_args().aws_user_to_update
 
-    ssl_verify = parser.parse_args().ssl_verify 
-    print "ssl_verify={}".format(ssl_verify)
+    session = boto3.Session(profile_name=aws_profile_name, region_name='us-east-1')
 
-    session = boto3.Session(profile_name=aws_profile_name)
+    jenkins_user = get_parameter_store_value(session=session, parameter_key=AWS_JENKINS_USER_PARAMETER_STORE)
+    jenkins_password = get_parameter_store_value(session=session, parameter_key=AWS_JENKINS_PASSWORD_PARAMETER_STORE)
+
     iam_client = session.client('iam')
-    j = Jenkins(baseurl='http://54.212.26.79:8080', username=jenkins_user, password=jenkins_password, ssl_verify=ssl_verify)
+    s3_client = session.client('s3')
+    j = Jenkins(baseurl='http://54.190.28.175:8080', username=jenkins_user, password=jenkins_password, lazy=True)
     all_users = get_all_users(iam=iam_client)
-    delete_keys(users=all_users, iam=iam_client, jenkins_conn=j, jenkins_credentials_description=jenkins_credentials_description, aws_user_to_update=aws_user_to_update)
+    delete_keys(users=all_users, iam=iam_client, jenkins_conn=j, jenkins_credentials_description=jenkins_credentials_description, aws_user_to_update=aws_user_to_update, s3_client=s3_client)
